a6c4cd392d225f749920d26f2aab51debb009c04
scrapgists <asishbm98@gmail.com>
2019-08-25 12:13:21 +0530

    logins are almost ok

---
+++ process.php
@@ -0,0 +1,29 @@
+<div class="container">
+  <form action="process.php" method="post">
+    <div class="form-group">
+      <label for="exampleInputEmail1">KTU ID</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter KTU login ID">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your id with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" required for="exampleCheck1">I agree</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
+<div class="container">
+  <form  action="main.html" method="post">
+      <div class="form-group">
+        <label for="exampleInputEmail1">KTU ID</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter KTU login ID">
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+      </div>
+  </form>
+</div>
